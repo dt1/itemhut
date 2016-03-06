de2ec9a8356dd6e2387da4fbd42eee64e4e6ec71
dolo <dbtoomey@gmail.com>
2016-03-06 02:43:56 -0600

    order tables

--- build_database/orders/orders.sql
+++ build_database/orders/orders.sql
@@ -12,18 +12,73 @@ create table orders.msku_sku (
 
 create table orders.market_orders (
        market_order_id varchar primary key,
-       marketplace varchar not null default 'testing',
-       marketplace_sku varchar not null,
-       qty_sold int check(qty_sold > 0),
+       marketplace varchar not null default 'main',
+       company_id int,
+       contact_id int,
        order_date date default now(),
        foreign key (marketplace_sku)
-               references orders.msku_sku (msku)
+               references orders.msku_sku (msku),
+       foreign key (company_id)
+               references company.companies (company_id)
+       foreign key (contact_id)
+               references company.contacts (contact_id)
 );
 
 create table orders.orders (
        order_id bigserial primary key,
        market_order_id varchar unique,
+       salesperson_id varchar not null,
+       foreign key (market_order_id)
+               references orders.market_orders (market_order_id)
+	       on update cascade,
+       foreign key (salesperson_id)
+               references users.users (user_id)
+	       on update cascade
+);
+
+create table orders.market_order_skus (
+       market_order_id varchar,
+       marketplace_sku varchar not null,
+       primary key (market_order_id, marketplace_sku)
+       foreign key (market_order_id)
+               references orders.market_orders (market_order_id)
+	       on update cascade,
+       foreign key (marketplace_sku)
+               references product.sku_upc (sku)
+);
+
+create table orders.price_per_piece (
+       market_order_id varchar,
+       marketplace_sku varchar not null,
+       price_per_piece numeric,
+       qty int check(qty > 0),
+       primary key(market_order_id, marketplace_sku,
+       	           price_per_piece, qty),
+       foreign key (market_order_id, marketplace_sku)
+               references orders.market_order_skus
+	       (market_order_id, marketplace_sku)
+);
+
+create table orders.shipto (
+       market_order_id varchar,
+       marketplace_sku varchar,
+       shipto_company varchar,
+       shipto_attn varchar,
+       shipto_street varchar,
+       shipto_city varchar,
+       shipto_state varchar,
+       shipto_zip varchar,
+       shipto_country varchar,
+       sku_qty int check(sku_qty > 0),
+       foreign key (market_order_id, marketplace_sku)
+               references orders.market_order_skus
+	       (market_order_id, marketplace_sku)
+);
+
+create table orders.files (
+       market_order_id varchar,
+       file_path varchar,
+       file_type varchar,
        foreign key (market_order_id)
                references orders.market_orders (market_order_id)
 );
-
