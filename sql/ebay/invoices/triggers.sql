d28700748dd78e349fa04482ca1bdee604cf37c3
dolo <dbtoomey@gmail.com>
2016-03-19 19:06:50 -0500

    cleanup

--- sql/ebay/invoices/triggers.sql
+++
@@ -1,4 +0,0 @@
-create trigger process_ebay_invoices
-after insert on ebay_invoices.json_insert
-for each row
-execute procedure ebay_invoices.process_json ();
--- sql/pydb/dbconn.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-import psycopg2
-
-conn = psycopg2.connect("dbname=itemhut user=postgres")
-
-cur = conn.cursor()
